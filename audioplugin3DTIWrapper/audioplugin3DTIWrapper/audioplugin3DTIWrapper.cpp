28ce453d0f4fd3e4b166d4529f1d24efd370cb2d
Carlos Garre <cgarre@uma.es>
2016-03-04 10:45:16 +0100

    First working version of the Unity Wrapper, with hardcoded HRTF and fixed listener

--- audioplugin3DTIWrapper/audioplugin3DTIWrapper/audioplugin3DTIWrapper.cpp
+++ audioplugin3DTIWrapper/audioplugin3DTIWrapper/audioplugin3DTIWrapper.cpp
@@ -1,17 +1,30 @@
 #include "AudioPluginUtil.h"
 
 #include "../../3DTI_Toolkit_Core/Core.h"
+#include "HRTFArray.h"
 
-#define LOG_FILE
+//#define LOG_FILE
 
-// Includes for reading HRTF data
-//#include <tchar.h>
-//#include <regex>
+// Includes for reading HRTF data and logging dor debug
 #include <fstream>
 #include <iostream>
 #include <time.h>
 
 using namespace std;
+
+// DEBUG LOG FILE
+template <class T>
+void WriteLog(string logtext, const T& value)
+{
+#ifdef LOG_FILE
+	ofstream logfile;
+	logfile.open("debuglog.txt", ofstream::out | ofstream::app);
+	logfile << logtext << value << endl;
+	logfile.close();
+#endif
+}
+
+/////////////////////////////////////////////////////////////////////
 
 namespace UnityWrapper3DTI
 {
@@ -29,46 +42,6 @@ namespace UnityWrapper3DTI
 		std::shared_ptr<CSingleSourceDSP> source;
 		CCore *core;
 	};
-
-/////////////////////////////////////////////////////////////////////
-
-	template <class T>
-	void WriteLog(string logtext, const T& value)
-	{
-#ifdef LOG_FILE
-		ofstream logfile;
-		logfile.open("debuglog.txt", ofstream::out | ofstream::app);
-		logfile << logtext << value << endl;
-		logfile.close();
-#endif
-	}
-
-//	class WriteLog
-//	{
-//	public:
-//		WriteLog(UnityAudioEffectState* _state) 
-//		{ 
-//			state = _state; 
-//#ifdef LOG_FILE
-//			state->GetEffectData<EffectData>()->logfile << logText << endl;
-//#endif
-//		}
-//		template <class T>
-//		WriteLog& operator<<(const T& value) {
-//			logText << value << padding;
-//			return *this;
-//		}
-//		//WriteLog& operator<<(std::ostream& (*func)(string&))
-//		//{
-//		//	func(logText);
-//		//	//state->GetEffectData<EffectData>()->logfile << logtext << endl;
-//		//	return *this;
-//		//}
-//	private:
-//		UnityAudioEffectState* state;		
-//		string logText;
-//		//std::ostringstream logText;
-//	};
 
 /////////////////////////////////////////////////////////////////////
 
@@ -100,144 +73,30 @@ namespace UnityWrapper3DTI
 	static UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK DistanceAttenuationCallback(UnityAudioEffectState* state, float distanceIn, float attenuationIn, float* attenuationOut)
 	{
 		// 3DTI: distance attenuation is included in ProcessAnechoic
+		//*attenuationOut = attenuationIn*(1.0f/distanceIn);
 		*attenuationOut = attenuationIn;
 		return UNITY_AUDIODSP_OK;
 	}
 
 /////////////////////////////////////////////////////////////////////
 
-	//void ReadHRIR(const TCHAR* fileName, CHRTF myHead)
-	//{
-	//	/* For each subject, there are 187 stereo 24 bits WAV files,
-	//	one for each position of the source. The generic name is:
-	//	IRC_<subject_ID>_<status>_R<radius>_T<azimuth>_P<elevation>.WAV */
-	//	std::regex patAz("_T[0-9]{3}"), patEl("_P[0-9]{3}"); // Pattern for azimuth and elevation.
-	//	string filename = fileName;		// WARNING!! This requires this project setting in VS: General->Character Set -> Multi-byte
-	//	std::sregex_iterator itAz(filename.begin(), filename.end(), patAz), itEl(filename.begin(), filename.end(), patEl);
-	//	string sAz = itAz->str(), sEl = itEl->str();
-	//	sAz.erase(0, 2); sEl.erase(0, 2);
-
-	//	// For the moment just load one HRIR, the one corresponding with current quantized azimuth and quantized elevation.
-	//	int tempAzimuth = stoi(sAz), tempElevation = stoi(sEl);
-
-	//	float* hrir_left, hrir_right; const int left = 0, right = 1;
-	//	hrir_left.load(file->getAbsolutePath(), left);
-	//	hrir_right.load(file->getAbsolutePath(), right);
-
-	//	// Take smallest
-	//	unsigned int length = (512 < hrir_left.length) ? 512 : hrir_left.length;
-	//	CMonoBuffer<float> hrir_left_float(length);
-	//	CMonoBuffer<float> hrir_right_float(length);
-	//	for (int i = 0; i < length; i++)
-	//	{
-	//		hrir_left_float[i] = static_cast<double>(hrir_left.temp[i]) / 32767.0; // cast from short
-	//	}
-	//	for (int i = 0; i < length; i++)
-	//	{
-	//		hrir_right_float[i] = static_cast<double>(hrir_right.temp[i]) / 32767.0;
-	//	}
-	//	HRIR_type onehrir = std::make_pair(hrir_left_float, hrir_right_float);
-	//	myHead.AddHRIR(tempAzimuth, tempElevation, std::move(onehrir));
-
-	//}
-
-/////////////////////////////////////////////////////////////////////
-
-	//CHRTF SetupHRTF()
-	//{
-	//	// Things to do for opening all files in a folder in Windows
-	//	WIN32_FIND_DATA FindFileData;
-	//	HANDLE hFind;
-	//	//TCHAR  *directory = TEXT("G:\\Work");
-	//	TCHAR *directory = TEXT(".\HRTFData");
-	//	TCHAR patter[MAX_PATH];
-	//	TCHAR fileName[MAX_PATH];
-	//	memset(patter, 0x00, MAX_PATH);
-	//	_stprintf(patter, TEXT("%s\\*.wav"), directory);
-	//	hFind = FindFirstFile(patter, &FindFileData);
-	//	if (hFind == INVALID_HANDLE_VALUE)
-	//	{
-	//		// ERROR!!!
-	//		return CHRTF();
-	//	}
-
-	//	// TO DO: Avoid these hardcoded constants
-	//	float azimuthStep = 15;
-	//	int buffersize = 512;
-
-	//	/// Start filling HRTF matrix
-	//	CHRTF myHead(azimuthStep, 15);
-	//	myHead.BeginSetup(buffersize); 
-
-	//	// Now read the wav files
-	//	do
-	//	{
-	//		//ignore current and parent directories
-	//		//if (_tcscmp(FindFileData.cFileName, TEXT(".")) == 0 || _tcscmp(FindFileData.cFileName, TEXT("..")) == 0)
-	//		//	continue;
-
-	//		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-	//		{
-	//			//ignore directories
-	//		}
-	//		else
-	//		{
-	//			//list the Files
-	//			_tprintf(TEXT("%s\n"), FindFileData.cFileName);
-	//			memset(fileName, 0x00, sizeof(fileName));
-	//			_stprintf(fileName, TEXT("%s\\%s"), directory, FindFileData.cFileName);
-	//			FILE *fptr = _tfopen((const TCHAR *)fileName, TEXT("r"));
-	//			
-	//			// Read one HRIR
-	//			ReadHRIR(fileName, myHead);				
-
-	//			fclose(fptr);
-	//			
-	//		}
-	//	} while (FindNextFile(hFind, &FindFileData));
-	//	FindClose(hFind);
-	//	
-	//	/// Stop filling HRTF matrix and load
-	//	myHead.EndSetup();
-	//}
-
 	CHRTF SetupHRTF()
 	{
-		// No error check
-		// This is a horrible hardcoded quick test
-		
-		// Create CHRTF object with needed size
-		//HRIR_type dummyHRIR;
-		//CHRTF dummyHRTF(15,15);
-		//dummyHRTF.BeginSetup(512);
-		//for (int i = 0; i < 187; i++)
-		//	dummyHRTF.AddHRIR(0.0f, 0.0f, move(dummyHRIR));
-		//dummyHRTF.EndSetup();
-		//int headsize = sizeof(dummyHRTF);
+		// From TXT File:
+		//CHRTF readHRTF;
+		//if (readHRTF.ReadFromFile("HRTF.txt") < 0)
+		//	WriteLog("	Could not open HRTF.txt file", "");
+		//else
+		//	WriteLog("	File HRTF.txt read!", "");
 
-		//// Read from binary file
-		//CHRTF* readHRTF;
-		//ifstream ifs("binaryHRTF.hrtf", ios::binary);
-		//ifs.read((char *)readHRTF, headsize);		
-		//ifs.close();
-		//
-		//ofstream logfile;
-		//logfile.open("debuglog.txt", ofstream::out | ofstream::app);
-		//logfile << "HRTF Read for 180,0: " << endl;
-		//CMonoBuffer<float> LeftChannel = readHRTF->GetInterpolatedHRIR(180, 0, true).first;
-		//for (int i = 0; i < 512; i++)
-		//	logfile << "	" << LeftChannel[i] <<  endl;
-		//logfile.close();
-
-		//return *readHRTF;
-
+		// From hardcoded array:
 		CHRTF readHRTF;
-		if (readHRTF.ReadFromFile("HRTF.txt") < 0)
-			WriteLog("	Could not open HRTF.txt file", "");
+		if (readHRTF.ReadFromArray(HRTFArray) < 0)
+			WriteLog("	Could not read HRTF array. ", "");
 		else
-			WriteLog("	File HRTF.txt read!", "");
-		
-		return readHRTF;		
+			WriteLog("	HRTF array read!", "");
+
+		return readHRTF;
 	}
 
 /////////////////////////////////////////////////////////////////////
@@ -251,7 +110,8 @@ namespace UnityWrapper3DTI
 			state->spatializerdata->distanceattenuationcallback = DistanceAttenuationCallback;	// TO DO: check if we can remove this
 		InitParametersFromDefinitions(InternalRegisterEffectDefinition, effectdata->p);			// TO DO: and this		
 
-		// Start log file
+		// DEBUG: Start log file
+#ifdef LOG_FILE
 		time_t rawtime;
 		struct tm * timeinfo;
 		char buffer[80];
@@ -260,6 +120,8 @@ namespace UnityWrapper3DTI
 		strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
 		std::string str(buffer);
 		WriteLog("***************************************************************************************\nDebug log started at ", str);
+#endif
+		//
 
 		// Core initialization
 		CCore *core = effectdata->core;		
@@ -283,7 +145,7 @@ namespace UnityWrapper3DTI
 		listenerTransform.SetOrientation(CQuaternion::UNIT);
 		listenerTransform.SetPosition(CVector3(0.0f, 0.0f, 0.0f));
 		core->SetListenerTransform(listenerTransform);
-		WriteLog("Listener transform set", "");
+		WriteLog("Listener transform set to fixed position: ", CVector3(0.0f, 0.0f, 0.0f));
 
 		// Set listener head circumference : TO DO
 
@@ -292,14 +154,6 @@ namespace UnityWrapper3DTI
 		CHRTF listenerHRTF = SetupHRTF();
 		core->LoadHRTF(std::move(listenerHRTF));
 		WriteLog("	Listener HRTF set.", "");
-
-		//ofstream logfile;
-		//logfile.open("debuglog.txt", ofstream::out | ofstream::app);
-		//logfile << "Listener HRTF for 180,0: " << endl;
-		//CMonoBuffer<float> leftChannel = listenerHRTF.GetInterpolatedHRIR(180, 0, 3).first;
-		//for (int i = 0; i < 512; i++)
-		//	logfile << "	" << leftChannel[i] << endl;
-		//logfile.close();
 
 		// TO DO: Setup room
 
@@ -320,9 +174,6 @@ namespace UnityWrapper3DTI
 	UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ReleaseCallback(UnityAudioEffectState* state)
 	{
 		EffectData* data = state->GetEffectData<EffectData>();
-#ifdef LOG_FILE
-		//data->logfile.close();
-#endif
 		delete data;
 		return UNITY_AUDIODSP_OK;
 	}
@@ -362,9 +213,7 @@ namespace UnityWrapper3DTI
 /////////////////////////////////////////////////////////////////////
 
 	UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ProcessCallback(UnityAudioEffectState* state, float* inbuffer, float* outbuffer, unsigned int length, int inchannels, int outchannels)
-	{
-		//WriteLog("Starting process with input buffer size (converted to MONO): ", length/2);
-
+	{		
 		// Check that I/O formats are right and that the host API supports this feature		
 		if (inchannels != 2 || outchannels != 2 ||
 			!IsHostCompatible(state) || state->spatializerdata == NULL)
@@ -379,27 +228,26 @@ namespace UnityWrapper3DTI
 		float* s = state->spatializerdata->sourcematrix;
 
 		// Set source position (we don't care about orientation)
-		float distanceScale = 0.0001f;
+		float distanceScale = 0.1f;
 		CTransform sourceTransform;
 		sourceTransform.SetPosition(CVector3(s[12]*distanceScale, s[13]*distanceScale, s[14]*distanceScale));
-		data->source->SetSourceTransform(sourceTransform);
-		//WriteLog("	Source position set to ", sourceTransform.GetPosition());
-
+		data->source->SetSourceTransform(sourceTransform);		
 		// We assume a fixed listener
 		
 		// Transform input buffer
-		// TO DO: Avoid this copy!!!!!!
-		CStereoBuffer<float> outStereoBuffer(length);
-		int monoLength = length / 2;
-		CMonoBuffer<float> inMonoBuffer(monoLength);
-		for (int i = 0; i < monoLength; i++)
+		// TO DO: Avoid this copy!!!!!!		
+		CMonoBuffer<float> inMonoBuffer(length);
+		for (int i = 0; i < length; i++)
 		{
-			inMonoBuffer[i] = inbuffer[i * 2];
+			inMonoBuffer[i] = inbuffer[i * 2]; // We take only the left channel
 		}
 
 		// Process!!
+		CStereoBuffer<float> outStereoBuffer(length * 2);
 		data->source->SetInterpolation(3);
-		data->source->ProcessAnechoic(inMonoBuffer, outStereoBuffer);				
+		data->source->UpdateBuffer(inMonoBuffer);
+		data->source->ProcessAnechoic(outStereoBuffer);
+		//data->source->ProcessAnechoic(inMonoBuffer, outStereoBuffer);				
 
 		// Transform output buffer
 		// TO DO: Avoid this copy!!!
