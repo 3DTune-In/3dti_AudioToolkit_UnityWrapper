c702e7b9909d8a3b06123f99af4abe8ef2f8ff5f
Carlos Garre <cgarre@uma.es>
2017-02-02 13:14:55 +0100

    Working in documentation

--- UnityWrapperPackageBuilder/Assets/TestScripts/TestHAAPI.cs
+++ UnityWrapperPackageBuilder/Assets/TestScripts/TestHAAPI.cs
@@ -16,6 +16,13 @@ public class TestHAAPI : MonoBehaviour {
         // Find main elements (API and mixer)
         HAAPI = Camera.main.GetComponent<API_3DTI_HA>();
         HAMIXER = HAAPI.haMixer;
+
+        // Switch on hearing aid for both ears
+        HAAPI.SwitchHAOnOff(API_3DTI_HA.EAR_BOTH, true);
+
+        // Add 12 bit quantization only at the input signal
+        HAAPI.SetQuantizationNoiseInChain(true, false);
+        HAAPI.SetQuantizationNoiseBits(12);
     }
 	
 	// Update is called once per frame
