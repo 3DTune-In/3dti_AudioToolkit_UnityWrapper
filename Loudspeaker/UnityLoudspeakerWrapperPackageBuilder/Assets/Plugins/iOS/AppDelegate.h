cc3304bdb8b4b78ab0770f035689752d95d6e6f0
Daniel Gonzalez Toledo <dgonzalezt@uma.es>
2017-05-05 19:18:34 +0200

    LoudSpekeakers wrapper: New independent package builder

---
+++ Loudspeaker/UnityLoudspeakerWrapperPackageBuilder/Assets/Plugins/iOS/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  
+//
+//  Created by Ragnar Hrafnkelsson on 01/03/2017.
+//
+//
+
+#import "UnityAppController.h"
+
+@interface AppDelegate : UnityAppController
+
+@end
---
+++ Loudspeaker/UnityLoudspeakerWrapperPackageBuilder/Assets/TestScripts/TestSpatializer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSpatializer : MonoBehaviour
+{
+    API_3DTI_Spatializer SpatializerAPI;
+    bool toggleSource = false;
+    AudioSource testSource;
+
+    void Start()
+    {
+        SpatializerAPI = Camera.main.GetComponent<API_3DTI_Spatializer>();
+        //SpatializerAPI.SetScaleFactor(1.0f);
+
+        testSource = GameObject.Find("TestAudio Source Left").GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space pressed");
+            toggleSource = !toggleSource;
+            if (toggleSource)
+            {
+                //testSource.spatialize = true;
+                //testSource.gameObject.SetActive(true);
+                testSource.Play();
+                SpatializerAPI.StartBinauralSpatializer(testSource);
+            }
+            else
+            {
+                testSource.Stop();
+                //testSource.gameObject.SetActive(false);
+                //testSource.spatialize = false;
+            }
+        }
+    }
+}
+
