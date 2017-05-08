787b30fda44a3e3fc2045c61ce023ce6cdf80274
Daniel Gonzalez Toledo <dgonzalezt@uma.es>
2017-05-08 11:05:33 +0200

    LoudSpeaker Wrapper: Update project to work in new folder. Clean project of Binaural files. Update Submodules

--- Loudspeaker/audioplugin3DTIToolkitLoudspeaker/src/PluginList.h
+++ Loudspeaker/audioplugin3DTIToolkitLoudspeaker/src/PluginList.h
@@ -1,4 +1,1 @@
-//DECLARE_EFFECT("3DTI Binaural Spatializer", Spatializer3DTI)
-//DECLARE_EFFECT("3DTI Hearing Aid Simulation", HASimulation3DTI)
-//DECLARE_EFFECT("3DTI Hearing Loss Simulation", HLSimulation3DTI)
 DECLARE_EFFECT("3DTI LoudSpeakers Spatializer", LoudspeakersSpatializer3DTI)
