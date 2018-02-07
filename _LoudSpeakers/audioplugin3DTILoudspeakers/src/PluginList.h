98bc778adeb5d40f66a62d33c81a51f0dcb5d784
mariacuevas <mariacuevas@uma.es>
2018-02-07 12:00:06 +0100

    Loudspeakers old folder removed

--- _LoudSpeakers/audioplugin3DTILoudspeakers/src/PluginList.h
+++
@@ -1,3 +0,0 @@
-DECLARE_EFFECT("3DTI Binaural Spatializer", Spatializer3DTI)
-DECLARE_EFFECT("3DTI Hearing Aid Simulation", HASimulation3DTI)
-DECLARE_EFFECT("3DTI Hearing Loss Simulation", HLSimulation3DTI)
