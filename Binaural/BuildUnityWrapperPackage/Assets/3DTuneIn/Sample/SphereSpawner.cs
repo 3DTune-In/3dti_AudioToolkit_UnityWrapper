472e5c7880b6f1a0ff9dcabebaf05ac446f89d33
timmb <tim@weiwei>
2021-01-21 17:49:26 +0000

    Update sample scene in BuildUnityWrapper project to include UI buttons.

--- Binaural/BuildUnityWrapperPackage/Assets/3DTuneIn/Sample/SphereSpawner.cs
+++ Binaural/BuildUnityWrapperPackage/Assets/3DTuneIn/Sample/SphereSpawner.cs
@@ -1,28 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SphereSpawner : MonoBehaviour
+public class SphereSpawner : MonoBehaviour, IPointerClickHandler
 {
     public GameObject spherePivot;
     public GameObject sphereEmitterPrefab;
     public bool spawnOnAwake;
     public bool spawnOnStart;
-    public bool spawnAfterSpatializerBinaryLoaded;
 
     private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
 
-    readonly int NumFrameDeltasToPrintOnSpawn = 3;
-    private int framesSinceSpawn = -1;
-
     private List<GameObject> spawnedSphereEmitters = new List<GameObject>();
-    private API_3DTI_Spatializer spatializer;
 
     public void Awake()
     {
         if (spawnOnAwake)
         {
-            spawn();
+            Spawn();
         }
     }
 
@@ -30,78 +26,48 @@ public class SphereSpawner : MonoBehaviour
     {
         if (spawnOnStart)
         {
-            spawn();
+            Spawn();
         }
 
         stopwatch.Start();
 
-        spatializer = Camera.main.GetComponent<API_3DTI_Spatializer>();
-        //// Set High Quality spatialization mode
-        //spatializer.SetSpatializationMode(API_3DTI_Spatializer.SPATIALIZATION_MODE_HIGH_QUALITY);
-        //// Load HRTF
-        //spatializer.LoadHRTFBinary("Assets/myData/myHRTF.3dti-hrtf");
-        //// Disable ILD, so that we don't need to load ILD resources
-        //spatializer.SetModNearFieldILD(false);
-        //// Enable hearing aid directionality for both ears
-        //// and set it to a moderate extend
-        //spatializer.SwitchOnOffHADirectionality(API_3DTI_Common.T_ear.BOTH, true);
-        //spatializer.SetHADirectionalityExtend(API_3DTI_Common.T_ear.BOTH, 15.0f);
-
-        if (spawnAfterSpatializerBinaryLoaded)
-        {
-            spawn();
-        }
     }
 
-    private void spawn()
+    public void Spawn()
     {
-        Debug.Log("Spawning new Sphere Emitter");
+        Debug.Log("Spawning new source (Sphere)");
         GameObject sphereEmitter = Instantiate(sphereEmitterPrefab, new Vector3(4, 0, 0), Quaternion.identity);
         sphereEmitter.transform.parent = spherePivot.transform;
-        //var spatializer = FindObjectOfType<API_3DTI_Spatializer>();
         var audioSource = sphereEmitter.GetComponent<AudioSource>();
         audioSource.Play();
-        //bool success = spatializer.StartBinauralSpatializer(audioSource);
-        //if (!success)
-        //{
-        //    Debug.LogWarning("Failed to start binaural spatializer on the new sphere", this);
-        //}
-        //FindObjectOfType<API_3DTI_Spatializer>().StartBinauralSpatializer(sphereEmitter.GetComponent<AudioSource>());
-
-
-        framesSinceSpawn = 0;
         spawnedSphereEmitters.Add(sphereEmitter);
-        Debug.Log($"{spawnedSphereEmitters.Count} spheres active.");
+        Debug.Log($"{spawnedSphereEmitters.Count} sources active.");
     }
 
-    public void Update()
+    public void RemoveSource()
     {
-        bool hasTouch = Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
-        if (Input.GetMouseButtonDown(0) || hasTouch)
+        if (spawnedSphereEmitters.Count > 0)
         {
-            spawn();
-        }
-        else if (Input.GetMouseButtonDown(1) && spawnedSphereEmitters.Count > 0)
-        {
-            Debug.Log("Removing oldest Sphere Emitter");
+            Debug.Log("Removing oldest Source");
             GameObject sphere = spawnedSphereEmitters[0];
             spawnedSphereEmitters.RemoveAt(0);
             Destroy(sphere);
         }
-
-        if (framesSinceSpawn >= 0 && framesSinceSpawn < NumFrameDeltasToPrintOnSpawn)
+        else
         {
-            //Debug.Log($"Frame delta: {stopwatch.ElapsedMilliseconds} ms");
-            framesSinceSpawn++;
+            Debug.Log("Cannot remove source as none exist.");
         }
-        stopwatch.Restart();
+    }
+
+    public void Update()
+    {
     }
 
 
 
     // Start is called before the first frame update
-    void OnMouseDown()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
     }
 }
