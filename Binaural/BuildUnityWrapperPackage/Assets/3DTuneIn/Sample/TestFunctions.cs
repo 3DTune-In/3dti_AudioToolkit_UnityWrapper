6db2915d94c9e192c7957a7804aac3e8ce4d1d13
timmb <772105+timmb@users.noreply.github.com>
2021-11-09 16:17:28 +0000

    Turn on toggles if sample scene launches with effect enabled

--- Binaural/BuildUnityWrapperPackage/Assets/3DTuneIn/Sample/TestFunctions.cs
+++ Binaural/BuildUnityWrapperPackage/Assets/3DTuneIn/Sample/TestFunctions.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using API_3DTI;
 using API_3DTI_Common;
+using UnityEngine.UI;
 
 public class TestFunctions : MonoBehaviour
 {
@@ -12,7 +13,23 @@ public class TestFunctions : MonoBehaviour
     public API_3DTI_Spatializer Spatializer;
     public SphereSpawner SphereSpawner;
 
+    public Toggle HearingAidToggle;
+    public Toggle HearingLossToggle;
+    public Toggle ReverbToggle;
+
     public int SoakTestMaxNumSources = 50;
+
+    private void Start()
+    {
+        // HA still uses the old API
+        if (HearingAid.haMixer.GetFloat("HA3DTI_Process_LeftOn", out float isLeftOnF) && HearingAid.haMixer.GetFloat("HA3DTI_Process_LeftOn", out float isRightOnF))
+        {
+            HearingAidToggle.SetIsOnWithoutNotify(isLeftOnF != 0.0f || isRightOnF != 0.0f);
+        }
+        // HL and Spatializer use the new API
+        HearingLossToggle.SetIsOnWithoutNotify(HearingLoss.GetParameter<bool>(API_3DTI_HL.Parameter.HLOn, T_ear.LEFT) || HearingLoss.GetParameter<bool>(API_3DTI_HL.Parameter.HLOn, T_ear.RIGHT));
+        ReverbToggle.SetIsOnWithoutNotify(Spatializer.GetParameter<bool>(API_3DTI_Spatializer.SpatializerParameter.EnableReverbProcessing));
+    }
 
     // Start is called before the first frame update
     void OnEnable()
